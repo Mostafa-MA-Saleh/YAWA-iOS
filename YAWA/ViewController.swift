a697d0c80a333170713852eaa0b174b430d5df45
Mostafa Saleh <mostafa.saleh@bugzy.net>
2017-07-18 19:51:10 +0200

    - Added a splash screen. - Added a check and message in case of request failure.

--- YAWA/ViewController.swift
+++ YAWA/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     //MARK: Properties
     let url = "http://api.openweathermap.org/data/2.5/forecast/daily?id=361058&units=metric&cnt=16&APPID=026ee82032707259db948706d2c48df2"
     var forcasts = [DayForcast]()
+    let progressHUD = ProgressHUD(text: "Please wait...")
     @IBOutlet weak var forcastTableView: UITableView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
@@ -24,8 +25,9 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(progressHUD)
         forcastTableView.dataSource = self
-        loadData()
+        requestData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,35 +36,51 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
 
     //MARK: Private Functions
-    private func loadData() {
-        Alamofire.request(url, method: .get) .responseJSON { response in
-            guard let json = response.result.value as? [String: Any] else {
-                fatalError("Error: \(response.result.error?.localizedDescription ?? "Error")")
+    private func loadData(_ response: DataResponse<Any>) {
+        guard let json = response.result.value as? [String: Any] else {
+            fatalError("Error: \(response.result.error?.localizedDescription ?? "Error")")
+        }
+        guard let list = json["list"] as? NSArray else {
+            fatalError("not a list")
+        }
+        for index in 0..<list.count{
+            guard let day = list[index] as? [String: Any] else {
+                fatalError("malformed json")
             }
-            guard let list = json["list"] as? NSArray else {
-                fatalError("not a list")
+            guard let temp = day["temp"] as? [String: Any] else {
+                fatalError("malformed json")
             }
-            for index in 0..<list.count{
-                guard let day = list[index] as? [String: Any] else {
-                    fatalError("malformed json")
-                }
-                guard let temp = day["temp"] as? [String: Any] else {
-                    fatalError("malformed json")
-                }
-                guard let weatherList = day["weather"] as? NSArray else {
-                    fatalError("malformed json")
-                }
-                guard let weather = weatherList[0] as? [String: Any] else {
-                    fatalError("malformed json")
-                }
-                let weekday = Calendar.current.component(.weekday, from: Date()) + index + 1
-                let dayForcast = DayForcast(weatherId: weather["id"]! as! Int, date: Utils.getDayOfWeek(number: weekday), forcast: weather["main"]! as! String, temp_max: temp["max"]! as! Int, temp_min: temp["min"]! as! Int)
-                if index == 0 {
-                    self.setupHeaderView(dayForcast)
-                } else {
-                    self.add(dayForcast)
-                }
+            guard let weatherList = day["weather"] as? NSArray else {
+                fatalError("malformed json")
             }
+            guard let weather = weatherList[0] as? [String: Any] else {
+                fatalError("malformed json")
+            }
+            let weekday = Calendar.current.component(.weekday, from: Date()) + index + 1
+            let dayForcast = DayForcast(weatherId: weather["id"]! as! Int, date: Utils.getDayOfWeek(number: weekday), forcast: weather["main"]! as! String, temp_max: temp["max"]! as! Int, temp_min: temp["min"]! as! Int)
+            if index == 0 {
+                self.setupHeaderView(dayForcast)
+            } else {
+                self.add(dayForcast)
+            }
+        }
+    }
+    private func requestData() {
+        progressHUD.show()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .failure(let error):
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Retry", style: .default, handler: { action in
+                    self.requestData()
+                }))
+                self.present(alert, animated: true, completion: nil)
+            case .success(_):
+                self.loadData(response)
+            }
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            self.progressHUD.hide()
         }
     }
     private func setupHeaderView(_ dayForcast: DayForcast) {
